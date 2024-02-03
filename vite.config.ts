fe09500a89d5699faf895d68e6b86396cbb234cc
gitteration <show10033@naver.com>
2024-02-03 17:29:19 +0900

    refactor: prettier 적용

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,13 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: {
-        port: 8000
-    },
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
+  server: {
+    port: 8000
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  }
 });
